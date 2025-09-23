0de0b61991787eef7c9d9876740409b39af21dfb
Roman Divkovic <Roman.Divkovic@Capio.se>
2025-09-23 22:18:04 +0200

    Fix failed pipeline with new eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,25 +2,35 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { configs as tsConfigs } from '@typescript-eslint/eslint-plugin'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  // Base JS recommended rules
+  js.configs.recommended,
+  // TypeScript recommended rules from @typescript-eslint
+  tsConfigs.recommended,
+  // TypeScript-specific overrides (applies to ts/tsx files)
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
       globals: globals.browser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@typescript-eslint': tsPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': 'off',
     },
-  }
-)
+  },
+]
